40f401dfdf394a56c34b13d0a4a11d4ab9f23f99
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-03 12:16:20 +0530

    7

---
+++ Logical_operator.java
@@ -0,0 +1,25 @@
+public class Logical_operator {
+    public static void main(String[] args) {
+
+        boolean a=true;
+        boolean b=false;
+        System.out.println("a&&b="+(a&&b));
+        System.out.println("a||b="+(a||b));
+        System.out.println("!(a||b)="+!(a||b));
+
+
+        boolean wearingGlass=false;
+        int hight=75;
+        char sex='M';
+
+        boolean canApply=!wearingGlass &&hight>65&&sex=='M';
+        System.out.println("Student can apply : "+canApply);
+
+
+        char camAState='O';
+        char camBState='H';
+        boolean canMonitor=camAState=='O'||camBState=='O';
+        System.out.println("Room can be monitored : "+canMonitor);
+        
+    }
+}
