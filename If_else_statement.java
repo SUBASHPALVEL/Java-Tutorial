83822b7bed76652aeb9b9006c79597058e05d014
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-03 12:29:15 +0530

    10

---
+++ If_else_statement.java
@@ -0,0 +1,12 @@
+public class If_else_statement {
+    public static void main(String[] args) {
+
+        int grade=10;
+        if(grade>=50){
+            System.out.println("Passed");
+        }else{
+            System.out.println("Failed");
+        }
+        System.out.println("Grade = "+grade);
+    }
+}
