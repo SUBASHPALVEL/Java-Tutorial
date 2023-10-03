ce0cef6d3cd9bc72a598187586cbb62c8a0705aa
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-03 12:20:53 +0530

    8

---
+++ Ternary_or_conditional_operator.java
@@ -0,0 +1,10 @@
+public class Ternary_or_conditional_operator {
+    public static void main(String[] args) {
+
+        int examGrade = 40;
+
+        String result = (examGrade >= 50) ? "Passed" : "Failed";
+        System.out.println("Result: " + result);
+
+    }
+}
