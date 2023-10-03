2fb7ff34fe5756393ccc9a895bb46e2510608d34
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-03 11:33:29 +0530

    3

---
+++ Arithmetic_Operator.java
@@ -0,0 +1,26 @@
+public class Arithmetic_Operator{
+    public static void main(String[] args) {
+
+
+        // ASSIGNMENTS
+        int numA = 5;
+        int numB = 10;
+
+        // OPERATIONS
+        int addition = numA + numB;
+        System.out.println("numA + numB = " + addition);
+
+        int subtraction = numA - numB;
+        System.out.println("numA - numB =" + subtraction);
+
+        int multiplication = numA * numB;
+        System.out.println("numA * numB =" + multiplication);
+
+        int division = numB / numA;
+        System.out.println("numB / numA = " + division);
+
+        int modulus = numB % numA;
+        System.out.println("numB % numA =" + modulus);
+
+    }
+}
