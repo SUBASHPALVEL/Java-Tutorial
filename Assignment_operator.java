efbf093d32ff289c34ea99e1da940a327696bdb4
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-03 12:18:45 +0530

    7

---
+++ Assignment_operator.java
@@ -0,0 +1,37 @@
+public class Assignment_operator {
+    public static void main(String[] args) {
+ 
+        int numA=11;
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"+=2 equals to "+(numA+=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"-=2 equals to "+(numA-=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"*=2 equals to "+(numA*=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"/=2 equals to "+(numA/=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"%=2 equals to "+(numA%=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"&=2 equals to "+(numA&=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"|=2 equals to "+(numA|=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+">>=2 equals to "+(numA>>=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+"<<=2 equals to "+(numA<<=2));
+
+        System.out.println("numA="+numA);
+        System.out.println(numA+">>>=2 equals to "+(numA>>>=2));
+
+    }
+}
