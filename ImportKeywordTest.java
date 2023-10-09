38a30fda937a46c2c4dd5e8651155511c0f4c317
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-09 11:08:13 +0530

    Import Statement

---
+++ ImportKeywordTest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Date;
+
+public class ImportKeywordTest {
+    public static void main(String[] args) {
+        
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        String numbersStr = Arrays.toString(numbers);
+        System.out.println(numbersStr);
+
+        Date myDate = new Date();
+        System.out.println(myDate.toString());
+
+    }
+}
