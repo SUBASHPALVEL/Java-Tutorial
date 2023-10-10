25e9a07b8899a0fe1263d6997131da998526d26f
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-10 10:08:20 +0530

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,55 @@
+public class Inheritance {
+    
+}
+
+
+// Single inheritance
+
+public class A{
+
+}
+
+public class B extends A{
+
+}
+
+// Multi level inheritance
+
+public class C{
+
+}
+
+public class D extends C{
+
+}
+
+public class E extends D{
+
+}
+
+// Hierarchical inheritance
+
+public class F {
+
+}
+
+public class G extends F{
+
+}
+
+public class H extends F{
+
+}
+
+// multiple inheritance does not supported in java, we have to use interface concept to make it possible
+
+
+
+
+
+
+
+
+
+
+
