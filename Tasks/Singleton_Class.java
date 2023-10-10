a67dcc6610412d95e25e3cd6164795d559906145
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-10 12:39:22 +0530

    Singleton Task

--- Tasks/Singleton_Class.java
+++ Tasks/Singleton_Class.java
@@ -1,29 +1,37 @@
-class A {
-    private static A instance;
-
-    private A() {
-
-    }
-
-    public static A getInstance() {
-        if (instance == null) {
-            instance = new A();
-        }
-        return instance;
+public class Singleton_Class {
+    public static void main(String[] args) {
+        B b = new B(); 
+        
+        C c = new C(); 
     }
 }
 
+class A {
+   
+}
+
 class B extends A {
+    private static boolean created = false;
+
+    public B() {
+        if (created) {
+            throw new IllegalStateException("Only one instance of B can be created.");
+        }
+        created = true;
+    }
+
 
 }
 
 class C extends A {
+    private static boolean created = false;
 
-}
-
-public class Singleton_Class {
-    public static void main(String[] args) {
-        B b = B.getInstance(); 
-        C c = C.getInstance(); 
+    public C() {
+        if (created) {
+            throw new IllegalStateException("Only one instance of C can be created.");
+        }
+        created = true;
     }
+
 }
+
