734de33944f3853b6a7eea4fd721944877dfdd91
SUBASHPALVEL <subashpalvel.ai@gmail.com>
2023-10-25 18:59:35 +0530

    TASK UPDATE

--- TasksList/ObjectComparatorWOSET.java
+++ TasksList/ObjectComparatorWOSET.java
@@ -1,11 +1,13 @@
 package TasksList;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.reflect.Field;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
-public class objectComparatorDifferentDataTypes {
+public class ObjectComparatorWOSET{
     public static void main(String[] args) {
         Internclass intern = new Internclass(1, "Subash", 123);
         Employeeclass employee = new Employeeclass("27", "Subash", 80000, 124);
@@ -18,7 +20,7 @@ public class objectComparatorDifferentDataTypes {
     public static void copy(Object source, Object target) {
         Field[] sourceFields = source.getClass().getDeclaredFields();
         Field[] targetFields = target.getClass().getDeclaredFields();
-        
+
         for (Field targetField : targetFields) {
             for (Field sourceField : sourceFields) {
                 AlternativeName altNameAnnotation = targetField.getAnnotation(AlternativeName.class);
@@ -27,10 +29,22 @@ public class objectComparatorDifferentDataTypes {
                 if (targetField.getName().equals(sourceField.getName()) || (altFieldName != null && altFieldName.equals(sourceField.getName()))) {
                     if (targetField.getType().equals(sourceField.getType())) {
                         try {
-                            targetField.setAccessible(true);
-                            sourceField.setAccessible(true);
-                            targetField.set(target, sourceField.get(source));
-                        } catch (IllegalAccessException e) {
+                            // Get the getter method for the source field
+                            String sourceFieldName = sourceField.getName();
+                            String sourceGetterName = "get" + sourceFieldName.substring(0, 1).toUpperCase() + sourceFieldName.substring(1);
+                            Method sourceGetter = source.getClass().getMethod(sourceGetterName);
+
+                            // Get the setter method for the target field
+                            String targetFieldName = targetField.getName();
+                            String targetSetterName = "set" + targetFieldName.substring(0, 1).toUpperCase() + targetFieldName.substring(1);
+                            Method targetSetter = target.getClass().getMethod(targetSetterName, sourceField.getType());
+
+                            // Get the value from the source object using the getter method
+                            Object sourceValue = sourceGetter.invoke(source);
+
+                            // Set the value in the target object using the setter method
+                            targetSetter.invoke(target, sourceValue);
+                        } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
