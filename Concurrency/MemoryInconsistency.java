465d0779a7c339ef582280fc9537f467bf37fb49
SUBASH PALVEL <subashpalvel.ai@gmail.com>
2023-11-06 07:12:53 +0530

    Concurrency

---
+++ Concurrency/MemoryInconsistency.java
@@ -0,0 +1,45 @@
+package Concurrency;
+
+public class MemoryInconsistency {
+
+    public static void main(String[] args)  {
+
+        Counter counter = new Counter();
+        Thread counterThreadA = new Thread() {
+            @Override
+            public void run() {
+                counter.increment();
+            }
+        };
+
+        Thread counterThreadB = new Thread() {
+            @Override
+            public void run() {
+                counter.decrement();
+            }
+        };
+
+        counterThreadA.start();
+        counterThreadB.start();
+
+        System.out.println("Counter = " + counter.getCounter());
+    }
+}
+
+class Counter {
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void increment() {
+        counter++;
+        System.out.println("counter value after increment=" + counter);
+    }
+
+    public void decrement() {
+        counter--;
+        System.out.println("counter value after decrement=" + counter);
+    }
+}
---
+++ Concurrency/SynchronizedStatement.java
@@ -0,0 +1,52 @@
+package Concurrency;
+
+class Counter {
+    private int counter = 0;
+
+    public synchronized int getCounter() {
+        return counter;
+    }
+
+    public void increment() {
+        synchronized (this) {
+            System.out.println("counter value before increment=" + counter);
+            counter++;
+            System.out.println("counter value after increment=" + counter);
+        }
+    }
+
+    public void decrement() {
+        synchronized (this) {
+            System.out.println("counter value before decrement=" + counter);
+            counter--;
+            System.out.println("counter value after decrement=" + counter);
+        }
+    }
+}
+
+public class SynchronizedStatement {
+
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread counterThreadA = new Thread() {
+            @Override
+            public void run() {
+                counter.increment();
+            }
+        };
+
+        Thread counterThreadB = new Thread() {
+            @Override
+            public void run() {
+                counter.decrement();
+            }
+        };
+
+        counterThreadA.start();
+        counterThreadB.start();
+
+        counterThreadA.join();
+        counterThreadB.join();
+        System.out.println("Counter = " + counter.getCounter());
+    }
+}
